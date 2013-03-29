9144609e83f2fc0f3ee6f804ec0e90e65a11c694
Kieu Anh Tuan <passkey1510@gmail.com>
2013-03-29 10:26:50 +0100

    Hot fix while waiting for PR https://github.com/Behat/MinkBrowserKitDriver/pull/14 to be merged

--- src/Behat/Mink/Driver/GoutteDriver.php
+++ src/Behat/Mink/Driver/GoutteDriver.php
@@ -415,7 +415,7 @@ class GoutteDriver implements DriverInterface
      */
     public function isChecked($xpath)
     {
-        return (bool) $this->getValue($xpath);
+        return null !== $this->getAttribute($xpath, 'checked');
     }
 
     /**
